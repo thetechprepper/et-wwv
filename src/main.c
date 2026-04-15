f722c515776a7c9a19f32fea2a114f203e8ce8ac
thetechprepper <info@thetechprepper.com>
2026-04-15 06:54:14 -0700

    Added audio input flags

--- src/main.c
+++ src/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
 
-int main(void) {
-    printf("et-wwv placeholder\n");
+int main(int argc, char *argv[]) {
+    const char *file_path = NULL;
+
+    static struct option long_options[] = {
+        {"file", required_argument, 0, 'f'},
+        {0, 0, 0, 0}
+    };
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "f:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'f':
+                file_path = optarg;
+                break;
+            default:
+                return 1;
+        }
+    }
+
+    if (file_path == NULL) {
+        return 1;
+    }
+
+    FILE *fp = fopen(file_path, "rb");
+    if (fp == NULL) {
+        return 1;
+    }
+
+    fclose(fp);
     return 0;
 }
